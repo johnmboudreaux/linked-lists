bb270f11c62eb8579b9c171025f03272bdbe5c04
johnmboudreaux <jhnbdrx@gmail.com>
2017-07-12 17:19:09 -0600

    clear read bookmarks button added and counters working

--- scripts.js
+++ scripts.js
@@ -1,30 +1,34 @@
 // (function() {
-
 var enterButton = $('#enterButton');
 var clearReadButton = ('#clearReadButton');
-var readButton= $('#card--read');
-var deleteButton = $('#card--delete');
+var readButton= $('#readButton');
+var deleteButton = $('#deleteButton');
 var bookMarks = $('#bookMarks');
 var titleInput = $('#titleInput');
 var urlInput = $('#urlInput');
 var totalCards = $('#totalCards');
 var readCards = $('#readCards');
 var unreadCards = $('#unreadCards');
-var cardCounter = 0;
 var readCardCounter = 0;
+var cardCounter = 0;
 var unreadCardCounter = 0;
 
 $( "#titleInput" ).keyup(function() {
   $("#enterButton").prop("disabled", !this.value);
-});
+    }
+  )
+;
 
 $( "#urlInput" ).keyup(function() {
   $("#enterButton").prop("disabled", !this.value);
-});
+    }
+  )
+;
 
 enterButton.on('click', function() {
   if ($('.form--input--title').val() === "" || $('.form--input--title--url').val() === "") {
     alert("please complete both fields before submitting");
+
 } else {
   var titleInput = $('#titleInput');
   var urlInput = $('#urlInput');
@@ -37,47 +41,71 @@ enterButton.on('click', function() {
     '<h2 class="card--title">' + titleInput.val() + '</h2>'+
     '<a href="' + urlInput.val() + '" class="card--link">' + urlInput.val() + '</a>'+
     '<div class="card--footer">'+
-      '<button class="card--footer--button card--read" id="readButton">Read</button>'+
+      '<button class="card--footer--button card--read" id="readButton">Mark As Read</button>'+
       '<button class="card--footer--button card--delete" id="deleteButton">Delete</button>'+
     '</div>'+
   '</div>'
-);}});
+          )
+        ;
+      }
+    }
+  )
+;
+
+// clear read bookmarks button event listener
+$('#clearReadButton').click(function(){
+  removeReadBookmarks();
+    }
+  )
+;
 
 // removes read bookmarks
-$('#clearReadButton').click(function(){
-    $('.button--read').remove();
-});
-
+function removeReadBookmarks(){
+  var cardsRemoved = $('.card--background--toggle').length;
+  $('.card--background--toggle').remove();
+    readCardCounter = 0;
+    cardCounter = cardCounter - cardsRemoved;
+    $('#readCards').html('Read Cards: ' + readCardCounter);
+    $('#totalCards').html('Card Count: ' + cardCounter);
+}
 
 // marks bookmark as read
 $(bookMarks).on('click', '#readButton', function(){
-  readCardCounter++;
-  unreadCardCounter--;
-  $('#readCards').html('Read Cards: ' + readCardCounter);
-  $('#unreadCards').html('Unread Card Count: ' + unreadCardCounter);
-  $(this).parent().closest('#wholeCard', '#readButton').toggleClass('button--read');
-    $(this).parent('#wholeCard', '#readButton').toggleClass('button--read');
-      $(this).css('color', 'red');
-});
+  if (!$(this).hasClass('button--read')) {
+    unreadCardCounter--;
+    readCardCounter++;
+    $(this).addClass('button--read');
+    $(this).closest('.card').addClass('card--background--toggle');
+  }else {
+    $(this).removeClass('button--read');
+    $(this).closest('.card').removeClass('card--background--toggle');
+    unreadCardCounter++;
+    readCardCounter--;
+  }
+    $('#readCards').html('Read Cards: ' + readCardCounter);
+    $('#unreadCards').html('Unread Card Count: ' + unreadCardCounter);
+    }
+  )
+;
 
 // deletes bookmark.  sets display to none.
-$(bookMarks).on('click', '#deleteButton', function(){
-  readCardCounter--;
-  cardCounter--;
-  $('#totalCards').html('Card Count: ' + cardCounter);
-  $('#readCards').html('Read Cards: ' + readCardCounter);
-  $(this).parent().parent().addClass('delete--box');
-});
-
-// function deleteCard() {}
+$(bookMarks).on('click', '#deleteButton', function(){ {
+  var readBtn = $(this).parent().children('.card--read');
+  if (readBtn.hasClass('button--read')) {
+    readCardCounter--;
+}else {
+    unreadCardCounter--;
+  }
 
 
-
-
-
-
-
-
+      cardCounter--;
+      $('#totalCards').html('Card Count: ' + cardCounter);
+      $('#readCards').html('Read Cards: ' + readCardCounter);
+      $('#unreadCards').html('Unread Card Count: ' + unreadCardCounter);
+      $(this).parent().parent().remove();
+    }}
+  )
+;
 
 
 
